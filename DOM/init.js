9993227a16db674d91605a5a8a0132aca8e37002
Pablo <paitorre@alumnos.ubiobio.cl>
2018-09-08 13:54:38 -0300

    uso de los metodos del objeto document

---
+++ DOM/init.js
@@ -0,0 +1,11 @@
+
+function div1OnAlert(){
+    var contenido = document.getElementById("div1").innerHTML;
+    alert(contenido);
+    
+}
+
+function div1Modifier(){
+    var contenido = document.getElementById("div1")
+    .innerHTML = '<p id="p1"> ...Cambio DIV...</p>';
+}
