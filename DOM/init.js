a179ce7beac3cdcf3c688b38da6e37b6f6363fbe
paitorre123 <paitorre@alumnos.ubiobio.cl>
2020-01-08 16:47:16 -0300

    Se generan dos funsiones en javascript para que muestren un determinadoo contenido y luego muestre el contenido original

--- DOM/init.js
+++ DOM/init.js
@@ -1,11 +1,25 @@
+let mensaje_anterior;
 
 function div1OnAlert(){
-    var contenido = document.getElementById("div1").innerHTML;
-    alert(contenido);
-    
+    let contenido = document.getElementById("div1").innerHTML;
+    alert("Mensaje: "+ contenido);
+
 }
 
-function div1Modifier(){
-    var contenido = document.getElementById("div1")
-    .innerHTML = '<p id="p1"> ...Cambio DIV...</p>';
+function modifierForward(){
+    mensaje_anterior = document.getElementById("div1").innerHTML;
+    document.getElementById("div1").innerHTML = '<p id="p1"> ...Cambio DIV...</p>';
+    document.getElementById( "modificador" ).
+    setAttribute( "onClick", "javascript: modifierBack();" );
+    //console.log(boton_alerta);
+    console.log("variable Contenida: " + mensaje_anterior);
+}
+
+
+function modifierBack() {
+  let contenido = document.getElementById("div1").innerHTML;
+  document.getElementById("div1").innerHTML = mensaje_anterior;
+  document.getElementById( "modificador" ).
+  setAttribute( "onClick", "javascript: modifierForward();" );
+
 }
